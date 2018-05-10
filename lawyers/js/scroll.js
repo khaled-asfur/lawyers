a2fdc79e977a08d7f8dac0030c54396d4cd3bf27
khaled-asfur <khalid_asfur@hotmail.com>
2018-05-10 20:23:05 +0300

    Revert "المشروع بعد الرفع على السيرفر المجاني يو"
    This reverts commit c2617a8a85712740de3df05148d25b790835c733.

--- lawyers/js/scroll.js
+++
@@ -1,9 +0,0 @@
-'use strict';
- /* global $, document,  */
-$(document).ready(function(){
-      $("body").niceScroll({
-             cursorcolor:"tomato",
-             cursorwidth:"7px"
-      });
-
-});
